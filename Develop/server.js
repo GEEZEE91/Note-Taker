07d819e14b01588f21af1192560f0ccc75df2428
GINA ZIVKOVIC <GINA.ZIVKOVIC91@GMAIL.COM>
2022-10-31 12:24:03 +0800

    update routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,38 +1,26 @@
 
 const express = require('express');
 const path = require('path');
-const database = require('./db/db.json');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 // Serve html, css files, js files from the public directory
 app.use(express.static('./public'));
 
 
-// Return db.json data at /api/Notes
-app.get("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            return res.json(JSON.parse(data));
-        }
-    })
-})
 
-
-
-
-
-
-
+//html and api route
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 
 app.listen(PORT, () => {
     console.log(`Starting server on http://localhost:${PORT}`);
+});
 });
